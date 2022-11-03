e701bbe4d380f5d890ab39fac9ccd5fe958181c0
Andre Koga <social@koga.one>
2022-11-03 18:02:52 -0400

    changed favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>KOGA</title>
+        <title>KOGA.ONE</title>
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
